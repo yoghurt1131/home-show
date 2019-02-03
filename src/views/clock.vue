cc908e4abbcae54c561cad6daf11f27db87b9d71
IshinFUKUOKA <yoghurt1131@gmail.com>
2019-02-03 23:09:30 +0900

    clockコンポーネントを切り出し

---
+++ src/views/clock.vue
@@ -0,0 +1,23 @@
+<template>
+    {{ now }}
+</template>
+
+<script>
+  export default {
+    name: "clock",
+    data: function() {
+      return {
+        now: (new Date).toFormat('MM/DD(DDD) HH:MI'),
+      }
+    },
+    created: function() {
+      setInterval(() => this.now =
+        (new Date).toFormat('MM/DD(DDD) HH:MI'), 1000 * 15
+      );
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
