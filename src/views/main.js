d98e15a8c8ad3190a758872d237a247358a1b3e3
IshinFUKUOKA <yoghurt1131@gmail.com>
2019-02-05 00:06:57 +0900

    eslint

--- src/views/main.js
+++ src/views/main.js
@@ -1,13 +1,13 @@
 const Vue = require('vue');
-let clock = require('./clock.vue');
-let weather = require('./weather.vue');
+const clock = require('./clock.vue');
+const weather = require('./weather.vue');
 window.addEventListener('load', function() {
   // crock
   new Vue({
     el: '#banner',
     components: {
       clock: clock,
-      weather: weather
-    }
+      weather: weather,
+    },
   });
 });
